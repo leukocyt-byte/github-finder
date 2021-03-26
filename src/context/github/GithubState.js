52858c2e1a84ad000befc68e6da80829a9bdb8dd
leukocyt-byte <58117573+leukocyt-byte@users.noreply.github.com>
2021-03-26 18:19:11 +0100

    refactoring

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -13,13 +13,9 @@ import {
 let githubClientId;
 let githubClientSecret;
 
-if(process.env.NODE_ENV !== "production") {
-    githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
-    githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
-} else {
-    githubClientId = process.env.GITHUB_CLIENT_ID;
-    githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
-}
+const [x, y] = process.env.NODE_ENV !== "production" ? 
+      [githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID, githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET] : 
+      [githubClientId = process.env.GITHUB_CLIENT_ID, githubClientSecret = process.env.GITHUB_CLIENT_SECRET];
 
 const GithubState = props => {
     const initialState = {
