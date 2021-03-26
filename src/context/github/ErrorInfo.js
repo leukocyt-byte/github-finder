178908ba488594d92d2b89d755114927affd5943
leukocyt-byte <58117573+leukocyt-byte@users.noreply.github.com>
2021-03-26 20:20:02 +0100

    Create ErrorInfo.js

---
+++ src/context/github/ErrorInfo.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const ErrorInfo = () => {
+    return (
+        <Fragment>
+        <div>
+            <h1>Error</h1>
+            <p className="lead">Sorry, something went wrong.</p>
+        </div>
+        </Fragment>
+    )
+}
+
+export default ErrorInfo;
